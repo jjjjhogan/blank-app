5d35fa0b6f498f03ce27ab2d1f11c831fc3daa5c
jjjjhogan <50223145+jjjjhogan@users.noreply.github.com>
2026-01-04 18:46:09 +0000

    number go up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,78 +1,20 @@
 import streamlit as st
-from openai import OpenAI
 
-import key as k
 """
-# THIS IS THE BATTLECATS HOW TO PAGE
+# Hello World, Streamlit!
 
-Are you bad at battlecats, well dont worry. Here is some tips on how to beat into the future chapter 2.
+This is a website to demonstrate Streamlit's API.
+You can stop looking at this now.
+
+Please.
 """
-def get_standard_response(system_prompt, user_prompt):
-    """
-    Sends a prompt to the ChatGPT API where it will return a standard response.
-    ChatGPT will not remember any prior conversations.
 
-    Parameters:
-    - system_prompt (str): Directions on how ChatGPT should act.
-    - user_prompt (str): A prompt from the user.
-
-    Returns:
-    - (str): ChatGPT's response.
-    """
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_prompt}
-        ]
-    )
-    return response.choices[0].message.content
+if 'number' not in st.session_state:
+    st.session_state['number'] = 0
 
 
-def talk_to_chatgpt(system_prompt):
-    """
-    Allows the user to have a conversation with the ChatGPT API.
-    ChatGPT will remember what the user says to it as long as this conversation is active.
-    When the function terminates, ChatGPT will forget everything the user said.
-    The user can end this conversation by typing and entering "STOP".
+clicked_button = st.button("Press me!")
+if clicked_button:
+    st.session_state['number'] += 1
 
-    Parameters:
-    - system_prompt (str): Directions on how ChatGPT should act.
-
-    Returns:
-    - (list(dict)): The chat history.
-    """
-
-    chat_history = [
-        {"role": "system", "content": system_prompt},
-    ]
-
-    while True:
-        user_prompt = input("Enter a response, or type in \"STOP\" to end this conversation. ")
-        if user_prompt == "STOP":
-            return chat_history
-        
-        chat_history.append(
-            {"role": "user", "content": user_prompt},
-        )
-        response = client.chat.completions.create(
-            model="gpt-4o",
-            messages = chat_history
-        )
-
-        assistant_response = response.choices[0].message.content
-        print(assistant_response)
-        print()
-
-        chat_history.append(
-            {"role": "assistant", "content": assistant_response}
-        )
-
-client = OpenAI(
-    api_key = k.Key
-)
-
-response = get_standard_response('You are a pro battlecats player, who gives tips to noobs','Design a team for into the future chapter 2')
-
-st.write(response)
-
+st.write(st.session_state['number'])
