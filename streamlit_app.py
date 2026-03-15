97de86c9fa74893e6d90a6265cbd3c6b26e68a6c
jjjjhogan <50223145+jjjjhogan@users.noreply.github.com>
2026-03-15 15:19:35 +0000

    """"""""

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ if 'name' not in st.session_state:
     st.session_state['name'] = name
 
 if st.session_state['name'] != '':
-    st.write(f'welcome {st.session_state['name']}')
+    st.write(f'welcome {st.session_state["name"]}')
