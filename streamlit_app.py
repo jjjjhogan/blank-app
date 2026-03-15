9ab9a4c4a8b06bbab002ffa1c12de8d297704933
jjjjhogan <50223145+jjjjhogan@users.noreply.github.com>
2026-03-15 15:22:10 +0000

    form

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,15 @@ import streamlit as st
 """
 # Hello what is your name!
 """
+with st.form('my-form'):
 
-name = st.text_input("Put your name")
+    name = st.text_input("Put your name")
 
 
-button = st.button('input name')
+    button = st.form_submit_button('input name')
 
-if button:
+    if button:
 
-    if 'name' not in st.session_state:
-        st.session_state['name'] = name
-    st.write(f'welcome {st.session_state["name"]}')
+        if 'name' not in st.session_state:
+            st.session_state['name'] = name
+        st.write(f'welcome {st.session_state["name"]}')
