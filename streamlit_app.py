3649f6c637dc3286253f3c1a2837d1722a845ec6
jjjjhogan <50223145+jjjjhogan@users.noreply.github.com>
2026-03-15 15:59:03 +0000

    while

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,41 +57,39 @@ You are a pokedex. Output in the form of json in this format: Put the values for
     }
     """
 with st.form('my_form'):
-    running = True
-    while(running):
-        # 3
-        inquiry = st.text_input("Welcome to the Pokedex!\nWhat would you like to do?\n1. View generated Pokedex entries\n2. Create a new Pokedex entry\n3. Exit with 'q' or 'e': ",key='test')
-        if inquiry == '1':
-            print('Here are all of your entries: ')
-            if len(st.session_state['pokedex'])==0:
-                print('You have no entries, use 2. Create a new Pokedex entry')
-            else:
-                for i in range(len(st.session_state['pokedex'])):
-                    st.write(str(i+1) + '. ' + str(st.session_state['pokedex'][i]['name']))
-                num = st.text_input('View which pokemon',key='view')
-
-                st.write(printdict(st.session_state['pokedex'][int(num)-1]))
-
-        elif inquiry == 'q' or inquiry == 'e':
-            running = False
+    # 3
+    inquiry = st.text_input("Welcome to the Pokedex!\nWhat would you like to do?\n1. View generated Pokedex entries\n2. Create a new Pokedex entry\n3. Exit with 'q' or 'e': ",key='test')
+    if inquiry == '1':
+        print('Here are all of your entries: ')
+        if len(st.session_state['pokedex'])==0:
+            print('You have no entries, use 2. Create a new Pokedex entry')
         else:
-            user_prompt = st.text_input('Type the pokemon you wish to add',key='add')
+            for i in range(len(st.session_state['pokedex'])):
+                st.write(str(i+1) + '. ' + str(st.session_state['pokedex'][i]['name']))
+            num = st.text_input('View which pokemon',key='view')
 
-            b = st.form_submit_button('Generate entry')
-            if b:
-                response = client.chat.completions.create(
-                    model="gpt-4o",
-                    response_format={ "type": "json_object" },
-                    messages=[
-                        {"role": "system", "content": system_prompt},
-                        {"role": "user", "content": user_prompt}
-                    ]
-                )
+            st.write(printdict(st.session_state['pokedex'][int(num)-1]))
+
+    elif inquiry == 'q' or inquiry == 'e':
+        running = False
+    else:            
+        user_prompt = st.text_input('Type the pokemon you wish to add',key='add')
+
+        b = st.form_submit_button('Generate entry')
+        if b:
+            response = client.chat.completions.create(
+                model="gpt-4o",
+                response_format={ "type": "json_object" },
+                messages=[
+                    {"role": "system", "content": system_prompt},
+                    {"role": "user", "content": user_prompt}
+                ]
+            )
 
                 # 5
 
-                st.write(printdict(loads(response.choices[0].message.content)))
-                st.session_state['pokedex'].append(loads(response.choices[0].message.content))      
+            st.write(printdict(loads(response.choices[0].message.content)))
+            st.session_state['pokedex'].append(loads(response.choices[0].message.content))      
     
 
 
