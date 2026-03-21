e2a7206959321d503d17a4c7dfbc08e2bb340f35
jjjjhogan <50223145+jjjjhogan@users.noreply.github.com>
2026-03-21 00:42:30 +0000

    n

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
+num = 0
 if 'num' not in st.session_state:
     st.session_state['num']=0
 
 button = st.button('Press me')
 if button:
-    st.session_state['num']+=1
-st.write(st.session_state['num'])
+    num+=1
+st.write(num)
