ee05fa25796849b4a0e5a966687b74e160bf4c8b
jjjjhogan <50223145+jjjjhogan@users.noreply.github.com>
2026-03-29 23:10:04 +0000

    2 forms

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
 import streamlit as st
-with st.chat_message("user",avatar="goku-1.png"):
-    st.write("Testing testing 123 123")
 
+name = ""
+dif = ""
+with st.form('name'):
+    name = st.text_input('Input name')
+    sub = st.form_submit_button('Submit name')
+    if sub:
+        st.write('sub')
+
+with st.form('diff'):
+    dif = st.text_input('Input name')
+    sub = st.form_submit_button('Submit name')
+
+    if sub:
+        st.write('sub')
+
+st.write('name: ' + name)
+
+st.write('dif: ' + dif)
