a75177db603d21b94d949c3be334e7df2966281b
jjjjhogan <50223145+jjjjhogan@users.noreply.github.com>
2026-03-29 23:02:21 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,3 @@
 import streamlit as st
-from st_chat_message import message
-
-message("Hi Spongebob", is_user=True)
-message("Hey there, I'm Spongebob Squarepants! How can I help you today?")
-
-if 'num' not in st.session_state:
-    st.session_state['num']=0
-
-button = st.button('Press me')
-if button:
-    st.session_state['num']+=1
-st.write(st.session_state['num'])
+with st.chat_message("user"):
+    st.write("Testing testing 123 123")
