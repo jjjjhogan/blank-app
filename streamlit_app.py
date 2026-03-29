7e2ced88a3bb04ce56f3adfbf8e9269a2407deca
jjjjhogan <50223145+jjjjhogan@users.noreply.github.com>
2026-03-29 23:04:51 +0000

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
-with st.chat_message("user",avatar=st.image("goku-1.png",width=50)):
+with st.chat_message("user",avatar="goku-1.png"):
     st.write("Testing testing 123 123")
 
