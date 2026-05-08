b6c4510ff5f23ee509b720652852fb2d3d1db8d8
jjjjhogan <50223145+jjjjhogan@users.noreply.github.com>
2026-05-08 00:18:27 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,41 @@
 import streamlit as st
 from openai import OpenAI
 import pandas as pd
+import random
 
 mode = st.selectbox('Choose mode',['Learn','Game','Sentence'])
-
+morse_code = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
+}
 if mode == "":
     st.write('Choose mode')
 elif mode == 'Learn':
-    st.write("Translate 'L'")
+    st.write(f"Translate {random.choice(list(morse_code.keys())) }")
     i = st.text_input("Put . and - with / inbetween for spaces")
 
 
